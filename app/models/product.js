dfee3de4816f758cdd2f140c5fa399ece19b4d63
Luca Marchi <luca.marchi91@gmail.com>
2015-10-17 12:16:21 +0200

    Product complete
    Aggiunti tutti i campi dei prodotti, manca la foto.

--- app/models/product.js
+++ app/models/product.js
@@ -3,7 +3,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
-	name: String
+	matricola: {type: Number,required: true},
+	materiale: {type: String, required: true},
+	cop: {type: String, enum: ['coil', 'pacco']},
+	quantita: {type: Number, default: 0},
+	dimenisioni: {type: String},
+	pesokg: {type: Number},
+	pesoton: {type: Number},
+	qualita: {type: String},
+	colore: {type: String},
+	ral: {type: String},
+	note: {type: String},
+	finitura: {type: String}
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
