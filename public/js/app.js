635a28ea8a475f7935e74251c85cf601487d0f60
FrancescoAmf <dcrfnc@gmail.com>
2015-11-11 00:11:32 +0100

    Form ancora in fase di sperimentazione

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,36 @@
 var myapp = angular.module('store', ["ui.router"])
-    myapp.config(function($stateProvider, $urlRouterProvider){
-      
-      // For any unmatched url, send to /route1
-      $urlRouterProvider.otherwise("/")
-      
-     	$stateProvider
-			.state('magazzino', {
-            	url: '/magazzino',
-            	templateUrl: 'magazzino.html',
-		  		    controller: 'productController'
-      })
-		  .state('carichiIn', {
-              url: "/carichiIn",
-              templateUrl: "carichientrata.html",
-              controller: 'orderController'
-	  	})
-      .state('addOrder', {
-              url: "/addOrder",
-              templateUrl: "order.html",
-              controller: 'orderController'
-      })
-	})
+myapp.config(function($stateProvider, $urlRouterProvider){
+
+	$urlRouterProvider.otherwise("/index.html")
+
+	$stateProvider
+		.state('magazzino', {
+			url: '/magazzino',
+			templateUrl: 'magazzino.html',
+			controller: 'productController'
+		})
+
+		.state('carichiIn', {
+			url: '/carichiIn',
+			templateUrl: 'carichientrata.html',
+			controller: 'orderController'
+		})
+
+		.state('productsList', {
+			url: '/productList',
+			templateUrl: 'orderproducts.html',
+			controller: 'orderController'
+		})
+
+		.state('createOrder', {
+			url: '/createOrder',
+			templateUrl: 'newOrder.html',
+			controller: 'orderController'
+		})
+
+		.state('createProduct', {
+			url: '/addProduct',
+			templateUrl: 'newProduct.html',
+			controller: 'productController'
+		})
+})
--- public/js/store.js
+++ public/js/store.js
@@ -5,8 +5,8 @@ store.factory('productFactory', function ($http) {
         get : function () {
             return $http.get('http://localhost:8080/api/products');
         },
-        post : function (product,orderId) {
-            return $http.post('http://localhost:8080/api/products/:orderId', product, orderId);
+        post : function (product, orderId) {
+            return $http.post('http://localhost:8080/api/products/' + orderId, product);
         },
         delete : function (id) {
             return $http.delete('http://localhost:8080/api/products/' + id);
@@ -14,34 +14,14 @@ store.factory('productFactory', function ($http) {
     };
 });
 
-store.controller('productController', function ($scope, productFactory) {
-    $scope.product = {orderId : "563655bb04d67032fff2c969"};
-    productFactory.get()
-        .success(function (data) {
-            $scope.products = data;
-            console.log('Lista prodotti ordine');
-        })
-        .error(function (data) {
-            console.log('Error: ' + data.message);
-        });
-
-    $scope.createProduct = function () {
-        $scope.orderId = "563655bb04d67032fff2c969";
-        productFactory.post($scope.product, $scope.orderId)
-            .success(function (data) {
-            $scope.product = {};
-            console.log('Prodotto Aggiunto ');
-            })
-            .error(function(data) {
-            console.log('Error: ' + data);
-            }); 
-    }
-});
-
-store.factory('orderFactory', function($http){
+store.factory('orderFactory', function ($http){
     return {
         get : function () {
             return $http.get('http://localhost:8080/api/orders');
+        },
+
+        getOrder: function(id) {
+            return $http.get('http://localhost:8080/api/orders/' + id);
         },
         post : function (order) {
             return $http.post('http://localhost:8080/api/orders/', order);
@@ -52,32 +32,98 @@ store.factory('orderFactory', function($http){
     };
 });
 
+store.controller('indexController', function ($scope, productFactory,orderFactory) {
+    $scope.getProducts = function() {
+		productFactory.get()
+        	.success(function (data) {
+            	$scope.products = data;
+            	console.log('Magazzino');
+        	})
+        	.error(function (data) {
+            	console.log('Error: ' + data.message);
+        	});
+	}
+	
+	$scope.getOrders = function () {
+        orderFactory.get()
+            .success(function (data) {
+                $scope.orders = data;
+                console.log('Lista Ordini');
+            })
+            .error(function (data) {
+                console.log('Error: ' + data.message);
+            });
+    }
+
+});
+
+store.controller('productController', function ($scope, productFactory) {
+    $scope.getProducts = function() {
+		productFactory.get()
+			.success(function (data) {
+				$scope.products = data;
+				console.log('Magazzino');
+			})
+			.error(function (data) {
+				console.log('Error: ' + data.message);
+			});
+	}
+	
+    $scope.createProduct = function () {
+       productFactory.post($scope.product, $scope.product.orderId)
+      		.success(function (data) {
+            	$scope.product = {};
+            	console.log('Prodotto Aggiunto ');
+            })
+            .error(function(data) {
+            	console.log('Error: ' + data.message);
+            }); 
+    }
+});
 
 store.controller('orderController', function ($scope, orderFactory) {
-    $scope.order = {};
-    orderFactory.get()
-        .success(function (data) {
-            $scope.orders = data;
-            console.log('Lista Ordini');
-        })
-        .error(function (data) {
-            console.log('Error: ' + data);
-        });
+    
+    $scope.getOrders = function () {
+        orderFactory.get()
+            .success(function (data) {
+                $scope.orders = data;
+                console.log('Lista Ordini');
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    }
+
+    $scope.productsList = function (id,nOrdine) {
+        orderFactory.getOrder(id)
+            .success(function(data) {
+                $scope.nOrdine = nOrdine;
+                $scope.productIds = data.productIds;
+                console.log("ID ORDINE ", id);
+                console.log("NUMERO ORDINE ", $scope.nOrdine);
+                console.log("PRODUCTSIDS ",	$scope.productIds);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    }
+    
+
 
     $scope.createOrder = function () {
         if ($scope.order != undefined) {
             if ($scope.order.numOrdine != null && $scope.order.ddt != null && $scope.order.fornitore != null) {
                 orderFactory.post($scope.order)
-                .success(function (data) {
-                    $scope.order = {};
-                    console.log('Ordine Creato');
-                    console.log(data);
-                })
-                .error(function (data) {
-                    console.log('Error: ' + data);
-                });
+					.success(function (data) {
+						$scope.order = {};
+						console.log('Ordine Creato');
+						console.log(data);
+					})
+					.error(function (data) {
+						console.log('Error: ' + data);
+					});
             }else{
-                alert("Compilare tutti i campi");
+                	alert("Compilare tutti i campi");
             }
         }
     }
